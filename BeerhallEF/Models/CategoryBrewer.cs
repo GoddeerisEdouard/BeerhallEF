afb24b7446bc0b5297d6c12b434ba5b8bc78f975
GoddeerisEdouard <goddeeris.edouard@student.hogent.be>
2020-12-29 01:51:11 +0100

    add Category and mapping

---
+++ BeerhallEF/Models/CategoryBrewer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeerhallEF.Models
+{
+    public class CategoryBrewer
+    {
+        #region Properties
+        public int BrewerId { get; set; }
+        public int  CategoryId { get; set; }
+        #endregion
+
+        #region Navigational Properties 
+        public Brewer Brewer { get; set; }
+        public Category Category { get; set; }
+        #endregion
+
+        #region Constructors
+        protected CategoryBrewer()
+        {
+        }
+
+        public CategoryBrewer(Category category, Brewer brewer) : this()
+        {
+            Category = category;
+            CategoryId = Category.CategoryId;
+
+            Brewer = brewer;
+            BrewerId = Brewer.BrewerId;
+        }
+        #endregion
+    }
+}
